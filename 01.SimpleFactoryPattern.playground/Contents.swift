d75db566f9a0880e807e0609843b88b69d903b0b
Su Xiaozhou <suxiaozhou@gmail.com>
2017-03-31 17:26:10 +0800

    simple factory pattern

---
+++ 01.SimpleFactoryPattern.playground/Contents.swift
@@ -0,0 +1,89 @@
+//: Playground - noun: a place where people can play
+
+class Operation{
+    private var _numberA: Double = 0
+    private var _numberB: Double = 0
+    
+    var numberA: Double{
+        get{
+            return _numberA
+        }
+        set{
+            _numberA = newValue
+        }
+    }
+    
+    var numberB: Double{
+        get{
+            return _numberB
+        }
+        set{
+            _numberB = newValue
+        }
+    }
+    
+    func GetResult() -> Double{
+        let result: Double = 0
+        return result
+    }
+}
+
+class OperationAdd: Operation{
+    override func GetResult() -> Double {
+        var result: Double = 0
+        result = numberA + numberB
+        return result
+    }
+}
+
+class OperationSub: Operation{
+    override func GetResult() -> Double {
+        var result: Double = 0
+        result = numberA - numberB
+        return result
+    }
+}
+
+class OperationMul: Operation{
+    override func GetResult() -> Double {
+        var result: Double = 0
+        result = numberA * numberB
+        return result
+    }
+}
+
+class OperationDiv: Operation{
+    override func GetResult() -> Double {
+        guard numberB != 0 else {
+            print("除数不能为 0")
+            return 0
+        }
+        var result: Double = 0
+        result = numberA / numberB
+        return result
+    }
+}
+
+class OperationFactory{
+    class func createOperate(operate: String) -> Operation?{
+        var oper: Operation?
+        
+        switch operate {
+        case "+":
+            oper = OperationAdd()
+        case "-":
+            oper = OperationSub()
+        case "*":
+            oper = OperationMul()
+        case "/":
+            oper = OperationDiv()
+        default: break
+        }
+        return oper
+    }
+}
+
+let op = OperationFactory.createOperate(operate: "+")
+op?.numberA = 1
+op?.numberB = 2
+op?.GetResult()
